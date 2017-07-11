35f042a32dfd76fed4041a5775f69b483466fb40
Kang Kang1 Chen <chenkang1@Lenovo.com>
2017-07-11 15:02:24 +0800

    add vs debug

--- src/api/images.route.js
+++ src/api/images.route.js
@@ -1,15 +1,31 @@
-import fs from 'fs'
+import fs from "fs";
 
-export default (app) => {
-    app.get('/download',(req,res)=>{
-        var img = fs.readFileSync('public/favicon.ico');
-        res.writeHead(200, {'Content-Type': 'image/gif' });
-        res.end(img, 'binary');
-    })
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root"
+});
 
-    app.get('/test',(req,res)=>{
-        var img = fs.readFileSync('public/test.js');
-        res.writeHead(200, {'Content-Type': 'application/x-javascript' });
-        res.end(img, 'binary');
-    })
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
+export default app => {
+  app.get("/download", (req, res) => {
+    var img = fs.readFileSync("public/favicon.ico");
+    res.writeHead(200, { "Content-Type": "image/gif" });
+    res.end(img, "binary");
+  });
+
+  app.get("/test", (req, res) => {
+    var img = fs.readFileSync("public/test.js");
+    res.writeHead(200, { "Content-Type": "application/x-javascript" });
+    res.end(img, "binary");
+  });
 };
