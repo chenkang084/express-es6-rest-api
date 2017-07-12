66c8b45996f1dd6e36eb8a2c317d506e9dd49973
Kang Kang1 Chen <chenkang1@Lenovo.com>
2017-07-12 10:52:28 +0800

    test sequelize

--- src/db.js
+++ src/db.js
@@ -1,18 +1,41 @@
+const Sequelize = require('sequelize');
+
+
 export default callback => {
   // connect to a database if needed, then pass it to `callback`:
-  var mysql = require("mysql");
-  var pool = mysql.createPool({
+  //   var mysql = require("mysql");
+  //   var pool = mysql.createPool({
+  //     host: "localhost",
+  //     user: "root",
+  //     password: "root",
+  //     database: "game",
+  //     connectionLimit: 10
+  //   });
+
+  //   pool.getConnection(function(err, connection) {
+  //     if (err) throw err;
+  //     console.log("db connected!");
+  //   });
+
+  const sequelize = new Sequelize("game", "root", "root", {
     host: "localhost",
-    user: "root",
-    password: "root",
-    database: "game",
-    connectionLimit: 10
+    dialect: "mysql",
+
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }
   });
 
-  pool.getConnection(function(err, connection) {
-    if (err) throw err;
-    console.log("db connected!");
-  });
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log("Connection has been established successfully.");
+    })
+    .catch(err => {
+      console.error("Unable to connect to the database:", err);
+    });
 
-  callback(pool);
+  callback(sequelize);
 };
