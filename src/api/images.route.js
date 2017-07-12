c43bfc2a2c555527e96945ccb69a8d3f900dc81d
Kang Kang1 Chen <chenkang1@Lenovo.com>
2017-07-12 11:38:41 +0800

    remove sequelize

--- src/api/images.route.js
+++ src/api/images.route.js
@@ -1,20 +1,5 @@
 import fs from "fs";
 import path from "path";
-const Sequelize = require("sequelize");
-
-  var mysql = require("mysql");
-  var pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "game",
-    connectionLimit: 10
-  });
-
-  pool.getConnection(function(err, connection) {
-    if (err) throw err;
-    console.log("db connected!");
-  });
 
 export default (app, db) => {
   app.get("/download", (req, res) => {
@@ -31,72 +16,9 @@ export default (app, db) => {
   });
 
   app.get("/db", (req, res) => {
-
-      // pool
-      // .query("SELECT * FROM game_usr_prfl WHERE NT_LOGIN = :NT_LOGIN", {
-      //   replacements: { NT_LOGIN: "kanchen" }
-      // })
-      // .then(projects => {
-      //   // console.log(projects)
-      //   res.send(projects);
-      // });
-
-      pool.query("SELECT * FROM game_usr_prfl WHERE NT_LOGIN = ?", {NT_LOGIN: 'kanchen'}, function (error, results, fields) {
+      db.query("SELECT * FROM game_usr_prfl WHERE NT_LOGIN = ?", {NT_LOGIN: 'kanchen'}, function (error, results, fields) {
           if (error) throw error;
           console.log(results.insertId);
       });
-  });
-
-  app.get("/insert", (req, res) => {
-    // db
-    //   .query("UPDATE game_usr_prfl SET SCR = 101 WHERE NT_LOGIN = :NT_LOGIN", {
-    //     replacements: { NT_LOGIN: "kanchen" }
-    //   })
-    //   .then(projects => {
-    //     // console.log(projects)
-    //     res.send(projects);
-    //   });
-
-    var SocialUrl = db.define(
-      "SocialUrl",
-      {
-        videoURL: Sequelize.STRING,
-        // artistId: Sequelize.STRING,
-        // type: Sequelize.STRING
-      },
-      {
-        tableName: "social_urls",
-        timestamps: false
-      }
-    );
-
-    SocialUrl.sync()
-      .then(() => {
-        // // Table created
-        // return SocialUrl.create({
-        //   videoURL: "abc",
-        //   artistId: "123",
-        //   type: "bbb"
-        // });
-      })
-      .then(() => {
-        SocialUrl.findAll().then(users => {
-          // console.log(users);
-
-          res.send(users)
-        });
-      });
-
-    // SocialUrl.create({
-    //   videoURL: "abc",
-    //   artistId: "123",
-    //   type: "bbb"
-    // }).complete(function(err, socialUrl) {
-    //   if (err) {
-    //     // log error;
-    //   } else {
-    //     console.log(socialUrl);
-    //   }
-    // });
   });
 };
--- src/db.js
+++ src/db.js
@@ -3,39 +3,19 @@ const Sequelize = require('sequelize');
 
 export default callback => {
   // connect to a database if needed, then pass it to `callback`:
-  //   var mysql = require("mysql");
-  //   var pool = mysql.createPool({
-  //     host: "localhost",
-  //     user: "root",
-  //     password: "root",
-  //     database: "game",
-  //     connectionLimit: 10
-  //   });
-
-  //   pool.getConnection(function(err, connection) {
-  //     if (err) throw err;
-  //     console.log("db connected!");
-  //   });
-
-  const sequelize = new Sequelize("game", "root", "root", {
-    host: "localhost",
-    dialect: "mysql",
-
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000
-    }
-  });
-
-  sequelize
-    .authenticate()
-    .then(() => {
-      console.log("Connection has been established successfully.");
-    })
-    .catch(err => {
-      console.error("Unable to connect to the database:", err);
+    var mysql = require("mysql");
+    var pool = mysql.createPool({
+      host: "localhost",
+      user: "root",
+      password: "root",
+      database: "game",
+      connectionLimit: 10
     });
 
-  callback(sequelize);
+    pool.getConnection(function(err, connection) {
+      if (err) throw err;
+      console.log("db connected!");
+    });
+
+  callback(pool);
 };
