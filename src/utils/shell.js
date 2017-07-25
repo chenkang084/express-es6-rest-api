d5963a1fc98a9993948f4d66c8fa527d1a0ab492
Kang Kang1 Chen <chenkang1@Lenovo.com>
2017-07-25 16:05:47 +0800

    add shell cb

--- src/utils/shell.js
+++ src/utils/shell.js
@@ -1,15 +1,11 @@
 var shell = require("shelljs");
 var iconv = require("iconv-lite");
 process.stdout.setEncoding("utf8");
-shell.exec(
-  "cd D:/ && cd test && mkdir test",
-  { encoding: "base64" },
-  (code, stdout, stderr) => {
-    // console.log(iconv.decode(iconv.encode(stdout, "base64"), "gb2312"));
-    // console.log(stdout);
 
-    if(stderr){
-        console.log('exec encountered error!')
+export default function exceShell(cmd, cb) {
+  shell.exec(cmd, { encoding: "base64" }, (code, stdout, stderr) => {
+    if (stderr) {
+      cb();
     }
-  }
-);
+  });
+}
